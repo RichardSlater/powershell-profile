2401abcf16c057875dce724a5cecff77456d7129
Richard Slater <github@richard-slater.co.uk>
2012-09-24 16:12:19 +0100

    Added method to kill Cassini

--- NuGet_profile.ps1
+++ NuGet_profile.ps1
@@ -1,4 +1,8 @@
 function SetPrefs(){
-get-project -all | %{ $_.Properties | ?{ $_.Name -eq "WebApplication.StartWebServerOnDebug" } | %{ $_.Value = "False"} }
-Write-Host "'Always Start When Debugging' has been disabled for all web projects."
+    Get-Project -all | %{ $_.Properties | ?{ $_.Name -eq "WebApplication.StartWebServerOnDebug" } | %{ $_.Value = "False"} }
+    Write-Host "'Always Start When Debugging' has been disabled for all web projects."
+}
+
+function KillCassini(){
+    Get-Process -Name WebDev.WebServer40 | Stop-Process
 }
