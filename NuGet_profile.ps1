6a3beec502778acdae377337ca6a4bd56df23cda
Richard Slater <richard.slater@amido.co.uk>
2013-01-28 11:03:06 +0000

    Added Fix-NugetPackages

--- NuGet_profile.ps1
+++ NuGet_profile.ps1
@@ -6,3 +6,20 @@ function SetPrefs(){
 function KillCassini(){
     Get-Process -Name WebDev.WebServer40 | Stop-Process
 }
+
+# http://stackoverflow.com/questions/13085480/restoring-nuget-references
+function Fix-NugetPackages
+{
+    foreach ($project in Get-Project -All) 
+    { 
+        $packages = Get-Package -ProjectName $project.ProjectName
+        foreach ($package in $packages) 
+        {
+            Uninstall-Package $package.Id -Force -ProjectName $project.ProjectName
+        }
+        foreach ($package in $packages) 
+        {
+            Install-Package $package.Id -ProjectName $project.ProjectName -Version $package.Version
+        }
+    }
+}
