6cfa9ebca89daaeebac03edd1972d7450c005c6a
Richard Slater <github@richard-slater.co.uk>
2016-06-30 16:54:45 +0100

    Updated Reinstall All Script

--- NuGet_profile.ps1
+++ NuGet_profile.ps1
@@ -1,17 +1,9 @@
 function Set-ProjectPreferences {
-    Get-Project -all | %{ $_.Properties | ?{ $_.Name -eq "WebApplication.StartWebServerOnDebug" } | %{ $_.Value = "False"} }
-    Write-Host "'Always Start When Debugging' has been disabled for all web projects."
+    Get-Project -all | %{ $_.Properties | ?{ $_.Name -eq "WebApplication.StartWebServerOnDebug" } | %{ $_.Value = "False"} };
+    Write-Host "'Always Start When Debugging' has been disabled for all web projects.";
 }
 
 # http://stackoverflow.com/questions/13085480/restoring-nuget-references
-function Fix-NugetPackages {
-    foreach ($project in Get-Project -All) { 
-        $packages = Get-Package -ProjectName $project.ProjectName
-        foreach ($package in $packages) {
-            Uninstall-Package $package.Id -Force -ProjectName $project.ProjectName
-        }
-        foreach ($package in $packages) {
-            Install-Package $package.Id -ProjectName $project.ProjectName -Version $package.Version
-        }
-    }
+function Repair-ProjectPackages {
+    Get-Project -All | Get-Package | Sort-Object -Unique Id, Version | Update-Package -Reinstall;
 }
