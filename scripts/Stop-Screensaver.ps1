4fc8a1aadc98c9de7a3697f9c6f0216594b12033
Richard Slater <github@richard-slater.co.uk>
2018-03-29 20:18:05 +0100

    Removed slow script.

--- scripts/Stop-Screensaver.ps1
+++
@@ -1,9 +0,0 @@
-[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
-
-function Stop-Screensaver ($minutes = 120) {
-  for ($i = 0; $i -lt $minutes; $i++) {
-    Start-Sleep -Seconds 60
-    $Pos = [System.Windows.Forms.Cursor]::Position
-    [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point((($Pos.X) + 5) , $Pos.Y)
-  }
-}
