ab4b3cdfd2ed2d94eaf24f6ec3bb51a4a5e2e4bb
Richard Slater <github@richard-slater.co.uk>
2020-03-11 13:16:43 +0000

    Update to Python 3

--- currency/Provider-Python3.ps1
+++ currency/Provider-Python3.ps1
@@ -1,5 +1,5 @@
 @{
-  Name = "Python 2";
+  Name = "Python 3";
   ActiveVersion = {
     $unparsedVersion = & python -c 'import sys; print(""."".join(map(str, sys.version_info[:3])))'
     $isInstalled = ($unparsedVersion | Where-Object { -Not [String]::IsNullOrWhiteSpace($_) }) -match '\d+\.\d+\.\d+'
@@ -14,7 +14,7 @@
     $ProgressPreference = 'SilentlyContinue'
     $updateCheck = Invoke-WebRequest -UseBasicParsing https://www.python.org/downloads/
     $ProgressPreference = $oldProgress
-    $downloadFile = $updateCheck.Links | Where-Object { $_.OuterHtml.Contains('Python 2') } | Select-Object -First 1 -ExpandProperty outerHtml
+    $downloadFile = $updateCheck.Links | Where-Object { $_.OuterHtml.Contains('Python 3') } | Select-Object -First 1 -ExpandProperty outerHtml
     $isInstalled = $downloadFile -match '\d+\.\d+\.\d+'
     if ($isInstalled) {
       return $matches[0]
